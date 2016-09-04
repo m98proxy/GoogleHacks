15a2c784277548d4cd500b736c872f0b13455bc8
Gerallt Franke <geralltf@gmail.com>
2016-09-04 19:55:41 +1000

    added model of a minimap

---
+++ GoogleHacks/GoogleHacks/Minimap.cs
@@ -0,0 +1,65 @@
+﻿/* Copyright © 2016
+ * Author: Gerallt Franke */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using X3D;
+
+namespace GoogleHacks
+{
+    /// <summary>
+    /// Displays a mini map in a 2D square depth-mask-locked to the top left hand corner of the screen,
+    /// showing nearest roads defined as LineSets; with points interpolated for smooth paths.
+    /// </summary>
+    public class Minimap
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Position of minimap in 3D space.
+        /// </summary>
+        public Vector3 Position { get; set; }
+
+        /// <summary>
+        /// Size of minimap in 3D space.
+        /// </summary>
+        public Vector3 Size { get; set; }
+
+        /// <summary>
+        /// Avatar position relative to minimap. Range: (0.0 - 1.0)
+        /// </summary>
+        public Vector3 AvatarPosition { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Minimap(Vector3 size, Vector3 position)
+        {
+            AvatarPosition = new Vector3(0.5f, 0.5f, 0.5f);
+
+            this.Size = size;
+            this.Position = position;
+        }
+
+        #endregion
+
+        #region Rendering Methods
+
+        public void Initilize()
+        {
+
+        }
+
+        public void Render(RenderingContext rc)
+        {
+
+        }
+
+        #endregion
+    }
+}
